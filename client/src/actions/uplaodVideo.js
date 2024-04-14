32019728301d4c34bd69196ecb3e58f0249caa7d
Utsavladia <124615886+Utsavladia@users.noreply.github.com>
2024-04-14 14:18:55 +0530

    .

--- client/src/actions/uplaodVideo.js
+++ client/src/actions/uplaodVideo.js
@@ -1,7 +1,10 @@
 import * as api from "../api";
+import {getAllVideos} from "./getAllVideos.js";
+
 export const uplaodVideo = (fileData, config) => async (dispatch) => {
   try {
     const { data } = await api.uplaodVideo(fileData, config);
+    dispatch(getAllVideos());
     console.log("we got the data on upload videos as ", data);
   } catch (error) {
     alert(error.response.data.message);
